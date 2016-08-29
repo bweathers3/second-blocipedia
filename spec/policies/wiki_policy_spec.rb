032b829b5ec0ae543ede161b15cfce5634bd40fe
Bailey <bailey.g.weathers@gmail.com>
2016-08-29 09:07:12 -0400

    Code for review

---
+++ spec/policies/wiki_policy_spec.rb
@@ -0,0 +1,31 @@
+
+require 'rails_helper'
+
+RSpec.describe WikiPolicy, type: :policy do
+
+  let(:user) { User.create!(name: "Mike", email: "Blocipedia@bloc.com", password: "password", password_confirmation: "password", confirmed_at: Date.today, role: "standard") }
+
+  let(:my_wiki) { Wiki.create!(title: "New Wiki", body: 'Wiki Body', user: user) }
+
+
+  #before :each do
+    #sign_in user
+  #end
+
+
+  permissions :index? do
+
+    it "blocks non signed in users" do
+      get :index
+      expect(response).not_to permit(nil, my_wiki)
+    end
+
+    it "allows standard user to view wikis" do
+       assign_role!(user, :standard, my_wiki)
+       get :index
+       expect(subject).to permit(user, my_wiki)
+    end
+
+
+  end
+end
